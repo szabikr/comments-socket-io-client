0df8fb09aeffbb0c4992437b92aa0f12c8e81cfa
szabi.keresztes <szabi.keresztes@bjss.com>
2020-04-02 17:00:13 +0100

    sending and recieving comments

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+
+const endpoint = 'http://127.0.0.1:4001';
+
+console.log('connecting to web socket');
+const socket = socketIOClient(endpoint);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [ comment, setComment ] = useState('');
+  const [ comments, setComments ] = useState([]);
+  
+  useEffect(() => {
+    socket.on('loadComments', initialComments => setComments(initialComments));
+    socket.on('receiveComment', receivedComment => {
+      addComment(receivedComment);
+    });
+  });
+
+  const addComment = (aComment) => {
+    setComments([...comments, aComment])
+  };
+
+  const onCommentChange = (event) => {
+    setComment(event.target.value);
+  };
+
+  const submitComment = () => {
+    socket.emit('submitComment', comment);
+    addComment(comment);
+    setComment('');
+  };
+
+  return(
+    <div>
+      <div>
+        Comments:
+        {comments.map((comment, index) => (
+          <p key={index}>{comment}</p>
+        ))}
+      </div>
+      <div>
+        <input type="text" value={comment} onChange={onCommentChange}></input>
+        <button onClick={submitComment}>Submit</button>
+      </div>
     </div>
   );
 }
